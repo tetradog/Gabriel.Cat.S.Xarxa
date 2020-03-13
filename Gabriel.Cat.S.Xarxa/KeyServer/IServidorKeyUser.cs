3db94d32b013b54d2fc968034bc418e64f1fe2a3
Gabriel <tetradog@gmail.com>
2020-03-13 18:53:40 +0100

    desarrollando servidor key :D

---
+++ Gabriel.Cat.S.Xarxa/KeyServer/IServidorKeyUser.cs
@@ -0,0 +1,14 @@
+﻿using Gabriel.Cat.S.Seguretat;
+using Gabriel.Cat.S.Utilitats;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gabriel.Cat.S.Xarxa
+{
+    public interface IServidorKeyUser
+    {
+        bool ContainsUser(IdUnico idUser);
+        Key GetKey(IdUnico idUser);
+    }
+}
