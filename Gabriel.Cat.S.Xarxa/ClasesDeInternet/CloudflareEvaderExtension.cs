f660c3e4c64001d31b45469d9f34978b54d881b3
Gabriel Cortés Garcia <tetradog@gmail.com>
2022-04-11 15:12:52 +0200

    commit

--- Gabriel.Cat.S.Xarxa/ClasesDeInternet/CloudflareEvaderExtension.cs
+++ Gabriel.Cat.S.Xarxa/ClasesDeInternet/CloudflareEvaderExtension.cs
@@ -10,11 +10,11 @@ namespace Gabriel.Cat.S.Extension
 {
     public static class CloudflareEvaderExtension
     {
-        public static Bitmap GetBitmapCloudflare([NotNull] this Uri url) => new Bitmap(new MemoryStream(url.GetDataCloudflare()));
-        public static byte[] GetDataCloudflare([NotNull] this Uri url) => url.GetCloudflareWebClient().DownloadData(url.AbsoluteUri);
-        public static WebClient GetCloudflareWebClient([NotNull] this Uri url) => Utilitats.ClasesDeInternet.CloudflareEvader.CloudflareWebClient(url);
-        public static string DownloadCloudflare([NotNull] this Uri url) => url.GetCloudflareWebClient().DownloadString(url.AbsoluteUri);
-        public static HtmlDocument LoadUrlCloudflare([NotNull] this HtmlDocument document, [NotNull] Uri url) => document.LoadString(url.DownloadCloudflare());
-        public static HtmlDocument LoadUrlCloudflare([NotNull] this HtmlDocument document, [NotNull] string url) => document.LoadUrlCloudflare(new Uri(url));
+        public static Bitmap GetBitmapCloudflare([NotNull] this Uri url, bool getNewClient = false, bool waitIfExist = true) => new Bitmap(new MemoryStream(url.GetDataCloudflare(getNewClient, waitIfExist)));
+        public static byte[] GetDataCloudflare([NotNull] this Uri url, bool getNewClient = false, bool waitIfExist = true) => url.GetCloudflareWebClient(getNewClient, waitIfExist).DownloadData(url.AbsoluteUri);
+        public static WebClient GetCloudflareWebClient([NotNull] this Uri url, bool getNewClient = false, bool waitIfExist = true) => Utilitats.ClasesDeInternet.CloudflareEvader.CloudflareWebClient(url, getNewClient, waitIfExist);
+        public static string DownloadCloudflare([NotNull] this Uri url, bool getNewClient = false, bool waitIfExist = true) => url.GetCloudflareWebClient(getNewClient, waitIfExist).DownloadString(url.AbsoluteUri);
+        public static HtmlDocument LoadUrlCloudflare([NotNull] this HtmlDocument document, [NotNull] Uri url, bool getNewClient = false, bool waitIfExist = true) => document.LoadString(url.DownloadCloudflare(getNewClient, waitIfExist));
+        public static HtmlDocument LoadUrlCloudflare([NotNull] this HtmlDocument document, [NotNull] string url, bool getNewClient = false, bool waitIfExist = true) => document.LoadUrlCloudflare(new Uri(url),getNewClient,waitIfExist);
     }
 }
